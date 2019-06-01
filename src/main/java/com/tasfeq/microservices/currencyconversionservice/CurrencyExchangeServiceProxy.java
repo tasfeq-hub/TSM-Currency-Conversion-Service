16af66f4439bc06e9b0793534e0677445150331d
Tasfeq <tasfeq123@gmail.com>
2019-06-01 06:23:40 +0200

    Ribbon load distribution added

--- src/main/java/com/tasfeq/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/tasfeq/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,10 +1,13 @@
 package com.tasfeq.microservices.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+//@FeignClient(name="currency-exchange-service", url="localhost:8000") [v1]
+@FeignClient(name="currency-exchange-service") // load distribution. no port is specified [v2]
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
