ac1dbdae3402f455a0eae0879004ecffed0bb56b
Tasfeq <tasfeq123@gmail.com>
2019-06-01 09:53:32 +0200

    Zuul implemented with Feign

--- src/main/java/com/tasfeq/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/tasfeq/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service", url="localhost:8000") [v1]
-@FeignClient(name="currency-exchange-service") // load distribution. no port is specified [v2]
+//@FeignClient(name="currency-exchange-service") // load distribution. no port is specified [v2]
+@FeignClient(name="netflix-zuul-api-gateway-server") // setting up Feign for Zuul API Gateway [v3]
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}") 
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}") //setting up Zuul API Gateway URI 
 	public CurrencyConversionBean retrieveExchangeValue(
 			@PathVariable("from") String from, 
 			@PathVariable("to") String to
