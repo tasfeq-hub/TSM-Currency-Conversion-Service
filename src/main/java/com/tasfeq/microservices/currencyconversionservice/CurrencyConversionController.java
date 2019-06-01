93be664914c68295961c2cb10046188661af86b4
Tasfeq <tasfeq123@gmail.com>
2019-06-01 06:01:35 +0200

    Feign proxy service added

--- src/main/java/com/tasfeq/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/tasfeq/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +13,10 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
-
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
+	
 	@GetMapping("/currency-converter/from/{from}/to/{to}/amount/{amount}")
 	public CurrencyConversionBean convertCurrency(
 			@PathVariable String from,
@@ -28,6 +32,27 @@ public class CurrencyConversionController {
 				uriVariables);
 		
 		CurrencyConversionBean response = responseEntity.getBody();
+		
+		return new CurrencyConversionBean(
+				response.getId(),
+				from,
+				to,
+				response.getConversionMultiple(),
+				amount,
+				amount.multiply(response.getConversionMultiple()),
+				response.getPort()
+				);
+	}
+	
+	// Solution with Feign //
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/amount/{amount}")
+	public CurrencyConversionBean convertCurrencyFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal amount) {
+		
+		
+		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
 		
 		return new CurrencyConversionBean(
 				response.getId(),
