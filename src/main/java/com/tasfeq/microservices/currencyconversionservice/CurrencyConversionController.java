ebcb9f6f1176feba0882cd4fc727923445081c34
Tasfeq <tasfeq123@gmail.com>
2019-06-02 10:44:15 +0200

    spring sleuth added

--- src/main/java/com/tasfeq/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/tasfeq/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ public class CurrencyConversionController {
 	
 	@Autowired
 	private CurrencyExchangeServiceProxy proxy;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/currency-converter/from/{from}/to/{to}/amount/{amount}")
 	public CurrencyConversionBean convertCurrency(
@@ -53,6 +57,8 @@ public class CurrencyConversionController {
 		
 		
 		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+		
+		logger.info("{}",response);
 		
 		return new CurrencyConversionBean(
 				response.getId(),
